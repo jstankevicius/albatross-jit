46452cb3d3f36612d41746cada1b53be300c3def
jstankevicius <juuuuuustas@gmail.com>
2023-07-14 16:09:08 -0400

    Add optional compilation of different compiler stages

--- src/albatross.cpp
+++ src/albatross.cpp
@@ -2,11 +2,18 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
 
 #include "lexer.h"
 #include "parser.h"
 #include "symres.h"
 #include "typecheck.h"
+
+#define COMPILE_STAGE_LEXER
+#define COMPILE_STAGE_PARSER
+#define COMPILE_STAGE_SYMBOL_RESOLVER
+#define COMPILE_STAGE_TYPE_CHECKER
+
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
@@ -26,13 +33,23 @@ int main(int argc, char *argv[]) {
   std::string content((std::istreambuf_iterator<char>(file)),
                       (std::istreambuf_iterator<char>()));
 
+#ifdef COMPILE_STAGE_LEXER
   ProgramText text(content);
 
   auto tokens = tokenize(text);
+
+#ifdef COMPILE_STAGE_PARSER
   auto stmts = parse_stmts(tokens);
 
+#ifdef COMPILE_STAGE_SYMBOL_RESOLVER
   SymbolTable st;
   st.enter_scope();
   resolve_stmts(stmts, st);
+
+#ifdef COMPILE_STAGE_TYPE_CHECKER
   typecheck_stmts(stmts);
+#endif
+#endif
+#endif
+#endif
 }
